48c045bc2f25e6f8cf62631a35222c9333c30204
Fran Ibarra <faibarra1@uc.cl>
2026-07-10 18:25:43 -0400

    feat: add base files and wire up the css

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import postcssGlobalData from "@csstools/postcss-global-data";
+import postcssCustomMedia from "postcss-custom-media";
+
+export default {
+  plugins: [
+    postcssGlobalData({ files: ["./src/styles/breakpoints.css"] }),
+    postcssCustomMedia(),
+  ],
+};
