4192fa4f174064c23dc10084f65ebd90a7d58425
Fran Ibarra <faibarra1@uc.cl>
2026-07-13 14:39:20 -0400

    fix: minor tweaks

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 import postcssGlobalData from "@csstools/postcss-global-data";
 import postcssCustomMedia from "postcss-custom-media";
+import postcssFluid from "postcss-fluid";
 
 export default {
   plugins: [
     postcssGlobalData({ files: ["./src/styles/breakpoints.css"] }),
     postcssCustomMedia(),
+    postcssFluid({ min: "360px", max: "1600px" }),
   ],
 };
