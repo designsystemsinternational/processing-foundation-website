2cf10d4107f06488d0d83f08c0aacb5ac37fc6da
Rune Skjoldborg Madsen <r@designsystems.international>
2026-08-21 21:02:56 +0200

    fix merge

--- src/components/primitives/Icons/index.ts
+++ src/components/primitives/Icons/index.ts
@@ -1,5 +1,5 @@
-export { default as ArrowIcon } from './ArrowIcon.astro';
-export type { Props as ArrowIconProps } from './ArrowIcon.astro';
+export { default as ArrowIcon } from './icons/ArrowIcon.astro';
+export type { Props as ArrowIconProps } from './icons/ArrowIcon.astro';
 export { default as ChevronIcon } from './icons/ChevronIcon.astro';
 export { default as FulltimeIcon } from './icons/FulltimeIcon.astro';
 export { default as PartTimeIcon } from './icons/PartTimeIcon.astro';
